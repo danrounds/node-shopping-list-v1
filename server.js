b611bca0f506053419faa868f49b15f771965b29
Dan Rounds <dan.rounds@gmail.com>
2016-12-27 17:51:48 -0500

    Add GET for /recipes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -18,10 +18,20 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+// add recipes
+Recipes.create('stew', ['lamb', 'carrots', 'parsnips', 'stock']);
+Recipes.create('cake', ['sugar', 'eggs', 'all-purpose flour', 'butter']);
+Recipes.create('bread', ['wheat flour', 'yeast', 'salt']);
+Recipes.create('caprese', ['tomato', 'mozzarella', 'basil', 'olive oil', 'balsamic vinegar']);
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
